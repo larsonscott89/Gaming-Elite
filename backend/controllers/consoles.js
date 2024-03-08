cdacb21cf7e468f6c4d6b8af8c363c9eb8a1afc2
larsonscott89 <scottlarson0305@gmail.com>
2024-03-08 10:42:47 -0500

    controllers and server boiler

---
+++ backend/controllers/consoles.js
@@ -0,0 +1,5 @@
+const Consoles = require('../models/consoles')
+
+module.exports = {
+  
+}
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const logger = require('morgan')
+const gameController = require('./controllers/games')
+const consoleController = require('./controllers/consoles')
+const brandController = require('./controllers/brands')
+const userController = require('./controllers/user')
+
+const db = require('./db')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(cors())
+app.use(logger('dev'))
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+// create
+
+
+// read
+
+
+// update
+
+
+// delete
