6dd92033feac4989f149084eddc5bcb7807a1de3
Lane17027 <lanenichols17027@gmail.com>
2024-03-08 09:01:14 -0600

    more boiler plate front end

--- frontend/gaming-elite/src/components/Main.jsx
+++ frontend/gaming-elite/src/components/Main.jsx
@@ -1,3 +1,10 @@
-export default function Accessories (){
+import Home from "./Home"
+import Consoles from "./Consoles"
+import Games from "./Games"
+import GameDetails from "./GameDetails"
+import Accessories from "./Accessories"
+import SearchResults from "./SearchResults"
+import UserSignup from "./UserSignup"
+export default function Main (){
     return
 }
--- frontend/gaming-elite/src/components/Nav.jsx
+++ frontend/gaming-elite/src/components/Nav.jsx
@@ -1,3 +1,3 @@
-export default function Accessories (){
+export default function Nav (){
     return
 }
