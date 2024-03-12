45da71ace5926656f6e5f1d026117bb696d89b84
larsonscott89 <scottlarson0305@gmail.com>
2024-03-12 14:54:33 -0400

    font fix

--- frontend/gaming-elite/src/components/Retro.jsx
+++ frontend/gaming-elite/src/components/Retro.jsx
@@ -30,7 +30,7 @@ export default function Retro() {
 
   return (
     <div>
-      <h1 className='retro-game'> Consoles </h1>
+      <h1 className='retro-game'> Games </h1>
       <div className="retro-card">
         {game.map((game, index) => (
           game.year_released <= 1995 && (
