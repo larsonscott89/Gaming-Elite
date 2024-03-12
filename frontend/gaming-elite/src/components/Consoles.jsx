2c7d507a7ce2ac80d1cf94fec5dce326a51c9037
Lane17027 <lanenichols17027@gmail.com>
2024-03-12 13:15:44 -0500

    changing consoles

--- frontend/gaming-elite/src/components/Consoles.jsx
+++ frontend/gaming-elite/src/components/Consoles.jsx
@@ -6,11 +6,16 @@ import "../style/Games.css";
 export default function Consoles() {
   const [consoles, setConsoles] = useState([]);
   const [ads, setAds] = useState([]);
+  const [brands, setBrands] = useState([]);
 
   const navigate = useNavigate();
-  let showItem = (id) => {
-    navigate(`/consoles/${id}`);
-  };
+//   let showItem = (id) => {
+//     navigate(`/consoles/${id}`);
+//   };
+
+let showBrand = (id) => {
+        navigate(`/brands/${id}`);
+      };
 
   useEffect(() => {
     const getConsoles = async () => {
@@ -26,25 +31,39 @@ export default function Consoles() {
     };
 
     getAds();
+
+    const getBrands = async () => {
+      const brandResponse = await axios.get("http://localhost:3001/brands");
+      setBrands(brandResponse.data);
+    };
+    getBrands()
   }, []);
+
+  for(let brand of brands){
+    if(brand.name=='Sony'){
+        brand.img_path="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/PlayStation_App_Icon.jpg/800px-PlayStation_App_Icon.jpg"
+    }
+    if(brand.name=='Microsoft'){
+        brand.img_path="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Xbox_app_logo.svg/1200px-Xbox_app_logo.svg.png"
+    }
+    if(brand.name=='Nintendo'){
+        brand.img_path="https://upload.wikimedia.org/wikipedia/commons/9/95/Nintendo_Logo_2017.png"
+    }
+    if(brand.name=='Sega'){
+        brand.img_path="https://fontmeme.com/images/Sega-Logo.jpg"
+    }
+    if(brand.name=='Atari'){
+        brand.img_path="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Atari_Official_2012_Logo.svg/1200px-Atari_Official_2012_Logo.svg.png"
+    }
+  }
+
+  console.log(brands)
 
   const pick1RandomAd = () => {
     const shuffledArray = ads.sort(() => 0.5 - Math.random());
     return shuffledArray.slice(0, 1);
   };
 
-  const mainConsoles = [];
-
-  for (let console of consoles) {
-    if (
-      console.name == "Playstation 5" ||
-      console.name == "Xbox Series X/S" ||
-      console.name == "Nintendo Switch" ||
-      console.name == "PC"
-    ) {
-      mainConsoles.push(console);
-    }
-  }
 
   return (
     <div className="games-page-container">
@@ -56,15 +75,15 @@ export default function Consoles() {
             </div>
           ))}
         </div>
-      </div>    
+      </div>
       <div className="platforms-container">
-        <h3>Browse Consoles for Sale</h3>
+        <h3>Browse Brands</h3>
         <div className="platform-cards-container">
-          {mainConsoles.map((item, index) => (
+          {brands.map((item, index) => (
             <div
               className="platform-card"
               key={item._id}
-              onClick={() => showItem(item._id)}
+              onClick={() => showBrand(item._id)}
             >
               <img className="platform-image" src={item.img_path} alt="" />
               <h3>{item.name}</h3>
