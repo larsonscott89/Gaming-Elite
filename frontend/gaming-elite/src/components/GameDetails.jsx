f2933dbf88c2b49319acd5be604ab30ed07678e3
kevin <xinjiewang2882@gmail.com>
2024-03-13 09:21:42 -0400

    15th

--- frontend/gaming-elite/src/components/GameDetails.jsx
+++ frontend/gaming-elite/src/components/GameDetails.jsx
@@ -48,50 +48,53 @@ export default function GameDetails ({ user_id }) {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
 
-    return(
-
-  
+    return (
         <div className="game-details-page">
-            <div className="game-details-container">
-                <div className="top-container">
-                    <div className="image-container">
-                        <img className='game-cover' src={game.img_path} alt="" />
+ 
+                <div className="game-details-container">
+                    <div className="top-container">
+                        <div className="image-container">
+                            <img className='game-cover' src={game?.img_path} alt="" />
+                        </div>
+                        <div className="details-section">
+                            <h2 className="game-title">{game?.title}</h2>
+                            <h2 className="game-price">${game?.price}</h2>
+                            <h3 className="game-release">Release Year: <span className="year-released">{game?.year_released}</span></h3>
+                            <h3 className="genres-title">Genres</h3>
+                            <h3 className="game-genres">{game?.genre}</h3>
+                            <img className='rating-img' src={game?.rating_img} alt=''/>
+                            <button onClick={addToCart}>Add to Cart</button>
+                        </div>
                     </div>
-                    <div className="details-section">
-                        <h2 className="game-title">{game.title}</h2>
-                        <h2 className="game-price"> ${game.price}</h2>
-                        <h3 className="game-release">Release Year: <span className="year-released">{game.year_released}</span></h3>
-                        <h3 className="genres-title">Genres</h3>
-                        <h3 className="game-genres">{game.genre}</h3>
-                        <img className='rating-img' src={game.rating_img} alt=''/>
-                        <button onClick={addToCart}>Add to Cart</button>
-                    </div>
-                </div>
-                <div className="description-container">
-                    <h3 className="description-title">Description</h3>
-                    <h4 className="game-description">{game.description}</h4>
-                    <div className="general-container">
-                        <h3 className="general-title">General</h3>
-                        <div className="general-info-container">
-                            <div className="general-info">
-                                <h4 className="game-players"> Number of Players</h4>
-                                <hr/>
-                                <h4 className="game-online">Online Multiplayer</h4>
-                                <hr/>
-                            </div>
-                            <div className="general-data">
-                                <h4 className="player-number">{game.number_of_players}</h4>
-                                <hr/>
-                                <div>
-                                    {game.online ? (
-                                        <h4 className="online-boolean">Yes</h4>
-                                    ):
-                                    <h4 className="online-boolean">No</h4>}
+                    <div className="description-container">
+                        <h3 className="description-title">Description</h3>
+                        <h4 className="game-description">{game?.description}</h4>
+                        <div className="general-container">
+                            <h3 className="general-title">General</h3>
+                            <div className="general-info-container">
+                                <div className="general-info">
+                                    <h4 className="game-players">Number of Players</h4>
                                     <hr/>
+                                    <h4 className="game-online">Online Multiplayer</h4>
+                                    <hr/>
+                                </div>
+                                <div className="general-data">
+                                    <h4 className="player-number">{game?.number_of_players}</h4>
+                                    <hr/>
+                                    <div>
+                                        {game?.online ? (
+                                            <h4 className="online-boolean">Yes</h4>
+                                        ) : (
+                                            <h4 className="online-boolean">No</h4>
+                                        )}
+                                        <hr/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
+            
+        </div>
+    );
+                                        }
