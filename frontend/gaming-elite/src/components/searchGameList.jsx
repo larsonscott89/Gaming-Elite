ee8c9588bf2903df80a0230ed3667edf6fcae2f2
connorreidy1 <connorjreidy@gmail.com>
2024-03-14 11:01:54 -0600

    searchConsoleList.jsx formatting and searchResults css

--- frontend/gaming-elite/src/components/searchGameList.jsx
+++ frontend/gaming-elite/src/components/searchGameList.jsx
@@ -24,31 +24,31 @@ const SearchGameList = ({ games }) => {
             <Link to={`/games/${game._id}`} onClick={handleClick}>
             <div className="search-card" key={game._id}>
                 <div className="search-image-container">
-                  <img className='search-game-image'
+                  <img className='search-image'
                     src={game.img_path}
                     alt={game.title}
                     onClick={handleClick}
                   />
                 </div>
                 <div className="search-details-container">
-                  <div className="search-game-title-container">
-                    <h2 className='search-game-title'>{game.title}</h2>
-                    <h3 className="search-game-price">${game.price}</h3>
+                  <div className="search-title-container">
+                    <h2 className='search-title'>{game.title}</h2>
+                    <h3 className="search-price">${game.price}</h3>
                   </div>
                   <div className="search-game-details">
-                    <div className='search-game-details-box'>
-                      <p className='search-game-release'>Release Year: <span className='search-year-released'>{game.year_released}</span></p>
+                    <div className='search-details-box'>
+                      <p className='search-release-title'>Release Year: <span className='search-year-released'>{game.year_released}</span></p>
                       <p className='search-genres-title'>Genres</p>
                       <p className='search-game-genres'>{game?.genre.join(', ')}</p>
                     </div>
-                    <div className='search-game-details-box'>
+                    <div className='search-details-box'>
                       <p className='search-game-players'>Number of Players: <span className='search-player-number'> {game.number_of_players}</span></p>
                       <div className='search-online-container'>
                         <p className='search-game-online'>Online</p>
                         <p>{game?.online ? ( <p className="search-online-boolean">Yes</p> ) : (<p className="search-online-boolean">No</p>)}</p>
                       </div>
                     </div>
-                    <div className='search-game-details-box'>
+                    <div className='search-details-box'>
                       <img className='search-rating-img' src={game?.rating_img} alt=''/>
                     </div>
                   </div>
