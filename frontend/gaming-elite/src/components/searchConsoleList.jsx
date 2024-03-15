bb7b9e44e4a34df08dc48187e1b45e3e591bd618
kevin <xinjiewang2882@gmail.com>
2024-03-15 09:01:47 -0400

    a

--- frontend/gaming-elite/src/components/searchConsoleList.jsx
+++ frontend/gaming-elite/src/components/searchConsoleList.jsx
@@ -11,38 +11,37 @@ const SearchConsoleList = ({ consoles }) => {
 
   return (
     <div className="search-page-container">
-        <div className="search-items-container">
-          {!hideList &&
-            consoles.map((console) => (
-              <Link to={`/consoles/${console._id}`} onClick={handleClick}>
-              <div className="search-card" key={console._id}>
-                <div className="search-image-container">  
-                  <img className="search-image"
-                    src={console.img_path}
-                    alt={console.title}
-                    onClick={handleClick}
-                  />
-                </div>
-                <div className="search-details-container">
-                  <div className='search-console-title-container'>
-                    <h2 className="search-title">{console.name}</h2>
-                  </div>
-                  <div className='search-details'>
-                    <div className='search-details-box'>
-                      <h3 className='search-price'>${console.price}</h3>
-                    </div>
-                    <div className='search-details-box'>
-                      <p className='search-brand-title'>Brand: <span className='search-brand'>{console.brand}</span></p>
-                      <p className='search-release-title'>Release Year: <span className='search-year-released'>{console.year_released}</span></p>
-                    </div>
-                  </div>
-                </div>   
+  <div className="search-items-container">
+    {!hideList &&
+      consoles.map((console) => (
+        <Link to={`/consoles/${console._id}`} onClick={handleClick} key={console._id}>
+          <div className="search-card">
+            <div className="search-image-container">  
+              <img className="search-image"
+                src={console.img_path}
+                alt={console.title}
+                onClick={handleClick}
+              />
+            </div>
+            <div className="search-details-container">
+              <div className='search-console-title-container'>
+                <h2 className="search-title">{console.name}</h2>
               </div>
-              </Link>
-          ))}
-        </div>
-    </div>
-  );
-};
+              <div className='search-details'>
+                <div className='search-details-box'>
+                  <h3 className='search-price'>${console.price}</h3>
+                </div>
+                <div className='search-details-box'>
+                  <p className='search-brand-title'>Brand: <span className='search-brand'>{console.brand}</span></p>
+                  <p className='search-release-title'>Release Year: <span className='search-year-released'>{console.year_released}</span></p>
+                </div>
+              </div>
+            </div>   
+          </div>
+        </Link>
+    ))}
+  </div>
+</div>
+)}
 
 export default SearchConsoleList;
--- frontend/gaming-elite/src/components/searchGameList.jsx
+++ frontend/gaming-elite/src/components/searchGameList.jsx
@@ -14,53 +14,52 @@ const SearchGameList = ({ games }) => {
   return (
     <div className="search-page-container">
       <div className="search-title-items-container">
-      {location.pathname.startsWith("/search") &&
-        <div className="search-title">
-          <h1>Search Results</h1>
-        </div>
+        {location.pathname.startsWith("/search") &&
+          <div className="search-title">
+            <h1>Search Results</h1>
+          </div>
         }
         <div className="search-items-container">
-        {!hideList &&
-          games.map((game) => (
-            <Link to={`/games/${game._id}`} onClick={handleClick}>
-            <div className="search-card" key={game._id}>
-                <div className="search-image-container">
-                  <img className='search-image'
-                    src={game.img_path}
-                    alt={game.title}
-                    onClick={handleClick}
-                  />
-                </div>
-                <div className="search-details-container">
-                  <div className="search-title-container">
-                    <h2 className='search-title'>{game.title}</h2>
-                    <h3 className="search-price">${game.price}</h3>
+          {!hideList &&
+            games.map((game) => (
+              <Link to={`/games/${game._id}`} onClick={handleClick} key={game._id}>
+                <div className="search-card">
+                  <div className="search-image-container">
+                    <img className='search-image'
+                      src={game.img_path}
+                      alt={game.title}
+                      onClick={handleClick}
+                    />
                   </div>
-                  <div className="search-details">
-                    <div className='search-details-box'>
-                      <p className='search-release-title'>Release Year: <span className='search-year-released'>{game.year_released}</span></p>
-                      <p className='search-genres-title'>Genres</p>
-                      <p className='search-game-genres'>{game?.genre.join(', ')}</p>
+                  <div className="search-details-container">
+                    <div className="search-title-container">
+                      <h2 className='search-title'>{game.title}</h2>
+                      <h3 className="search-price">${game.price}</h3>
                     </div>
-                    <div className='search-details-box'>
-                      <p className='search-game-players'>Number of Players: <span className='search-player-number'> {game.number_of_players}</span></p>
-                      <div className='search-online-container'>
-                        <p className='search-game-online'>Online</p>
-                        <p>{game?.online ? ( <p className="search-online-boolean">Yes</p> ) : (<p className="search-online-boolean">No</p>)}</p>
+                    <div className="search-details">
+                      <div className='search-details-box'>
+                        <p className='search-release-title'>Release Year: <span className='search-year-released'>{game.year_released}</span></p>
+                        <p className='search-genres-title'>Genres</p>
+                        <p className='search-game-genres'>{game?.genre.join(', ')}</p>
+                      </div>
+                      <div className='search-details-box'>
+                        <p className='search-game-players'>Number of Players: <span className='search-player-number'> {game.number_of_players}</span></p>
+                        <div className='search-online-container'>
+                          <p className='search-game-online'>Online</p>
+                          <p>{game?.online ? ( <p className="search-online-boolean">Yes</p> ) : (<p className="search-online-boolean">No</p>)}</p>
+                        </div>
+                      </div>
+                      <div className='search-details-box'>
+                        <img className='search-rating-img' src={game?.rating_img} alt=''/>
                       </div>
                     </div>
-                    <div className='search-details-box'>
-                      <img className='search-rating-img' src={game?.rating_img} alt=''/>
-                    </div>
                   </div>
                 </div>
-            </div>
-            </Link>
-          ))}
-          </div>
+              </Link>
+            ))}
         </div>
+      </div>
     </div>
   );
-};
-
+            }
 export default SearchGameList;
