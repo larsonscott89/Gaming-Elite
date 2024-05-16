41398831c99e06cfac9d79f5613865dc54595744
larsonscott89 <scottlarson0305@gmail.com>
2024-05-16 17:20:50 -0400

    trying to deploy

--- frontend/gaming-elite/src/main.jsx
+++ frontend/gaming-elite/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import CartProvider from './CartContext.jsx';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
